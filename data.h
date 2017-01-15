c0d5e1cca40e7b491509dc98e96e3ab97f65fb2c
Xuqiang Fang <danyfang7@gmail.com>
2017-01-15 17:02:33 +0100

    add header file

---
+++ data.h
@@ -0,0 +1,23 @@
+//
+//  data.h
+//  main
+//
+//  Created by Xuqiang Fang on 15/01/2017.
+//  Copyright © 2017 Xuqiang Fang. All rights reserved.
+//
+
+#ifndef data_h
+#define data_h
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+
+void datagene(int N_par, int n_par);
+
+#endif /* data_h */
+
